cc06af54b0808bb447aa016d411351b6da0803a6
Joey Huab <joey@evolution-x.org>
2020-12-10 19:40:40 +0530

    Add OP8T Cyberpunk 2077 FOD icon
    AshutoshSundresh: Also revert c3c1509

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -32,6 +32,7 @@ import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
@@ -381,6 +382,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
             @Override
             protected void onDraw(Canvas canvas) {
                 if (mIsCircleShowing) {
+                    mPaintFingerprintBackground.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
                     canvas.drawCircle(mSize / 2, mSize / 2, mSize / 2.0f, mPaintFingerprint);
                 }
                 super.onDraw(canvas);
